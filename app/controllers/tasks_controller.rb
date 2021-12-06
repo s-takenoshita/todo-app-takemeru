5bbc0bc05f220d8d1fae47731ca687cd91c5403c
s-takenoshita <s.takenoshita@gmail.com>
2021-12-06 22:06:51 +0900

    taskの修正と削除を実装。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,13 +24,32 @@ class TasksController < ApplicationController
   end
 
   def edit
+    board = Board.find(params[:board_id])
+    @task = board.tasks.find(params[:id])
   end
 
   def update
+    board = Board.find(params[:board_id])
+    @task = board.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to board_task_path(board.id, @task.id), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    board = Board.find(params[:board_id])
+    task = board.tasks.find(params[:id])
+    task.destroy!
+    redirect_to board_path(params[:board_id]), notice: '削除に成功しました'
   end
 
   private
+
   def task_params
     params.require(:task).permit(:title, :content, :deadline, :board_id)
-end
+  end
+
 end
